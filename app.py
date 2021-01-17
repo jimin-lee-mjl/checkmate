62011fcd60d412642dc81a658ee0c537ed39e4a9
Jungin <jungin3486@gmail.com>
2021-01-18 03:33:46 +0900

    hello

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index():
   return render_template('home.html')
 
+@app.route('/calendar')
+def hello():
+    return render_template("calendar.html")
+
+
 if __name__=="__main__":
   app.run(debug=True)
