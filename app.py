661878aaae3a425df49ecad0402c96c42d77d7aa
jimin <jiminaleejung@gmail.com>
2021-01-19 00:51:40 +0900

    enabled login and logout with flask-login

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from login import LoginForm, SignupForm, User
+from login import LoginForm, SignupForm, User, users
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
-Bootstrap(app)
+bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'secrtidsfjo222'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  if len(users) == 1:
+    return users[0]
+  else:
+    return users[1]
 
 @app.route('/') # 접속하는 url
 def index():
@@ -18,9 +30,10 @@ def index():
 def signup():
   form = SignupForm()
   if form.validate_on_submit():
-    new_user = User(form.username.data, form.email.data, form.password.data)
+    hashed_pw = generate_password_hash(form.password.data, method='sha256')
+    new_user = User(form.username.data, form.email.data, hashed_pw)
     new_user.create_user()
-    return new_user.user_db[0]
+    return redirect('/login')
 
   return render_template('signup_tp.html', form=form)
 
@@ -28,13 +41,28 @@ def signup():
 def login():
   form = LoginForm()
   if form.validate_on_submit():
-    return f"hi your username is {form.username.data} and password is {form.password.data}"
+    user = [x for x in users if x.username == form.username.data][0]
+    if user:
+      if check_password_hash(user.password, form.password.data):
+        login_user(user, remember=form.remember_me.data)
+        return redirect('/dashboard')
+      else:
+          return "Invalid password"
+    # if IndexError:
+    #   return redirect('/signup') This part should be handled later
 
   return render_template('login_tp.html', form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect('/')
+
 @app.route('/dashboard')
+@login_required
 def dashboard():
-  return "login"
+  return render_template('dash_tp.html', name = current_user.username)
 
 if __name__=="__main__":
   app.run(debug=True)
--- login.py
+++ login.py
@@ -1,25 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-# flask_wtf, flask_bootstrap, (email_validator)
+from flask_login import UserMixin
+
+# flask-wtf, flask-bootstrap, (email_validator), flask-login
 
 users = []
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember_me = BooleanField('remember me')
 
 class SignupForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-class User():
+class User(UserMixin):
     def __init__(self, username, email, password):
+        self.id = len(users) + 1
         self.username = username
         self.email = email
         self.password = password
-        self.user_db = []
     def create_user(self):
-        self.user_db.append({'username':self.username, 'email':self.email, 'password':self.password})
+        users.append(self)
+
+user1 = User('elice', 'elice@elice.com', 'rabbithole')
+user1.create_user()
