e966a03b03c1cd4d232c19976be07a7daaf5eaad
Jungin <jungin3486@gmail.com>
2021-01-19 11:10:09 +0900

    add calendar.html

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import json
 
-#Flask 객체 인스턴스 생성
 app = Flask(__name__)
-
-@app.route('/') # 접속하는 url
+Bootstrap(app)
+@app.route('/')
 def index():
   return render_template('home.html')
 
 @app.route('/calendar')
-def hello():
+def calendar():
     return render_template("calendar.html")
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+
+    with open("events.json", "r") as input_data:
+        return input_data.read()
+# @app.route('/calendar')
+# def hello():
+#     return render_template("/calendar/calendar.html")
 
 
 if __name__=="__main__":
