937902174342f2b8370f254b79a770b34318a8f5
sodam <sodam@LAPTOP-MTOF3AII.localdomain>
2021-01-25 21:50:29 +0900

    modify & rename (todo_personal->tasks)

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 # from config import DB_CONNECT
 
-
+#current_app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://elice:1234@localhost:3306/elice"
 # current_app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://elice:miniproject2@localhost:3306/elice"
 current_app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{DB_CONNECT['username']}:{DB_CONNECT['password']}@{DB_CONNECT['server']}:3306/{DB_CONNECT['dbname']}"
 current_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
