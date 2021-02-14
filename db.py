34b015dca5d7a28b7c2504bd1e9dbb1972456c76
jimin <jiminaleejung@gmail.com>
2021-02-14 22:44:30 +0900

    handled encoding issue

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 from config import DB_CONNECT
 from sqlalchemy.orm import relationship
 
-current_app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{DB_CONNECT['username']}:{DB_CONNECT['password']}@{DB_CONNECT['server']}:3306/{DB_CONNECT['dbname']}"
+current_app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{DB_CONNECT['username']}:{DB_CONNECT['password']}@{DB_CONNECT['server']}:3306/{DB_CONNECT['dbname']}?charset=utf8"
 current_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(current_app)
 
